1842bedbbc5cc6de991f2a1f3423bde87f0ee416
Apple <mido_2ofa@hotmail.com>
2015-03-26 18:17:41 +0200

    created system admin using active admin gem and now can sign in

--- website/config/application.rb
+++ website/config/application.rb
@@ -22,5 +22,6 @@ module Website
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.override_gem_home = false
   end
 end
