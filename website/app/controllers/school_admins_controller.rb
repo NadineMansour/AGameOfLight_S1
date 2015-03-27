72f37dda611bf0062efd34728012896bef8b08cf
Mariam Mohamed Fawzy <maryammohamed61@gmail.com>
2015-03-27 14:31:23 +0200

    add some documentation on the controller

--- website/app/controllers/school_admins_controller.rb
+++ website/app/controllers/school_admins_controller.rb
@@ -2,10 +2,13 @@ class SchoolAdminsController < ApplicationController
 
 
 	def view_verified_students
+		# get the school admin
 		@current_admin = SchoolAdmin.find(params[:id])
 		@students = Student.where("school = ? AND verified = ?" , 
 			@current_admin.school, true)
+		# get the sorting method if available
 		@method = params[:sort_method]
+		# sort only if there sre students and if the school admin wants to sort
 		if @method && @students
 			if @method == "1"
 				@students = @students.order(grade: :asc)
