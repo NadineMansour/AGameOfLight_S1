2f962e06d0b135aa2ba84ce3dfa92e7cb2b9899d
Mariam Mohamed Fawzy <maryammohamed61@gmail.com>
2015-03-27 15:42:52 +0200

    mariam: add logs to records #42

--- website/db/schema.rb
+++ website/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326205228) do
+ActiveRecord::Schema.define(version: 20150327133805) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150326205228) do
     t.integer  "clicks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "logs"
   end
 
   create_table "school_admins", force: :cascade do |t|
