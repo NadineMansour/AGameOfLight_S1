aae9d92d860bee3082740073bc79f6c7073d1d27
Nadine Mansour <nadinemansour94@gmail.com>
2015-03-28 19:15:58 +0200

    rotate the player and the light beam in Level 3-1

---
+++ LightGame/Assets/Scripts/R1_Level5.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class R1_Level5 : MonoBehaviour
+{
+	public float StartTime;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartTime = Time.realtimeSinceStartup;
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// not needed now
+		/*if (Time.realtimeSinceStartup - StartTime < 3.0f) 
+		{
+			Component halo = GetComponent ("Halo"); 
+			halo.GetType ().GetProperty ("enabled").SetValue (halo, true, null);
+		} 
+		else 
+		{
+			Component halo = GetComponent ("Halo"); 
+			halo.GetType ().GetProperty ("enabled").SetValue (halo, false, null);
+		}*/
+	}
+
+
+	//called when the rotate anti clockwise button is pressed 
+	public void OnMouseDown() 
+	{
+		Player_Level5_nadine.RRightTrue();
+	}
+	
+
+	//called when the rotate anti clockwise button is released  
+	public void OnMouseUp()
+	{
+		Player_Level5_nadine.RRightFalse ();
+	}
+}
