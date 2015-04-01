1c62a7912b12ea840d085d2a7eda5f3bd5cee4a2
MMelouk94 <mmelouk94@gmail.com>
2015-04-01 19:32:42 +0200

    changed the redirection after level 2

--- LightGame/Assets/Scripts/nextButton_MM_2.cs
+++ LightGame/Assets/Scripts/nextButton_MM_2.cs
@@ -3,18 +3,23 @@ using System.Collections;
 
 public class nextButton_MM_2 : MonoBehaviour {
 
+
 	public GameObject Tip1;
+
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 
+
 	void OnMouseDown()
 	{
 		if (ShooterScript_2.state == 2) 
 		{
+			//Uncomment following level and remove the level 5 one when level 3 is done.
 			//Application.LoadLevel("Level3");
+			Application.LoadLevel("Level5");
 		}
 		if (ShooterScript_2.state == 0) 
 		{
@@ -23,7 +28,8 @@ public class nextButton_MM_2 : MonoBehaviour {
 		}
 
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
 	
