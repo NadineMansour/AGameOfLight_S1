98f1b3a07bfdef68b4c56bdb03eca125f55272f5
MMelouk94 <mmelouk94@gmail.com>
2015-04-21 15:47:50 +0200

    Light travels in rays quiz

---
+++ LightGame/Assets/Scripts/next_Quiz1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class next_Quiz1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		if(Quiz1.state == 0)
+		{
+			Quiz1.state++;
+		}
+
+
+		if(Quiz1.state == 2)
+		{
+			Application.LoadLevel("Level3");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
