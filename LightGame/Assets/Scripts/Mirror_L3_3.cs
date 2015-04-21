77e9a351251d0c8b21dbc8400dabb09ced53dcbd
MMelouk94 <mmelouk94@gmail.com>
2015-04-22 00:23:44 +0200

    Level 3_3 V1.0, mirror movement

---
+++ LightGame/Assets/Scripts/Mirror_L3_3.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Mirror_L3_3 : MonoBehaviour {
+
+
+	public static bool MirrorL;
+	public static bool MirrorR;
+
+
+	void Start () {
+		MirrorL = false;
+		MirrorR = false;
+	}
+
+	
+	void Update () {
+		//if(ShooterScript....
+		//{
+			if (MirrorR && transform.position.x < 1) 
+			{
+				transform.position = transform.position + new Vector3(0.05f, 0, 0); 
+			}
+			if (MirrorL && transform.position.x > -5) 
+			{
+				transform.position = transform.position - new Vector3(0.05f, 0, 0); 
+			}
+
+		//}
+	}
+
+
+	public static void moveMirror(int x)
+	{
+		//Go Right
+		if (x == 0) {
+			MirrorR = true;
+		}
+		//Go Left
+		if (x == 1) {
+			MirrorL = true;
+		}
+		//Stop Right
+		if (x == 2) {
+			MirrorR = false;
+		}
+		//Stop Left
+		if (x == 3) {
+			MirrorL = false;
+		}
+	}
+}
