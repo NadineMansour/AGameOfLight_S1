da616e7776a096c80b1e47fdbc69809151ec5a98
Mariam Mohamed Fawzy <maryammohamed61@gmail.com>
2015-04-21 18:59:48 +0200

    mariam: Teacher: View list of verified students in school I am verified as teacher in (Sorted by grade, alphabetically or other options) (T3) #107

--- website/app/controllers/teachers_controller.rb
+++ website/app/controllers/teachers_controller.rb
@@ -13,4 +13,40 @@ class TeachersController < ApplicationController
 			@students = {}
 		end
 	end
+
+
+	def view_school_verified_students
+		@current_teacher = current_teacher
+		# get the sorting method if available
+		@method = params[:sort_method]
+		@order = params[:order_method]
+		if @current_teacher && @current_teacher.verified
+			@students = Student.where("school = ? AND verified = ?" , 
+				@current_teacher.school, true)
+			# sort only if there sre students and if the school admin wants to sort
+			if @method && @students && @order
+				if @method == "1" #sort by grade
+					if @order == "1"
+						@students = @students.order(grade: :asc)
+					else
+						@students = @students.order(grade: :desc)
+					end
+				elsif @method == "2" #sort by name
+					if @order == "1"
+						@students = @students.order(student_name: :asc)
+					else
+						@students = @students.order(student_name: :desc)
+					end
+				elsif @method == "3" #sort by signing up date
+					if @order == "1"
+						@students = @students.order(created_at: :asc)
+					else
+						@students = @students.order(created_at: :desc)
+					end
+				end
+			end
+		else
+			@students = {}
+		end
+	end
 end
--- website/test/controllers/teachers_controller_test.rb
+++ website/test/controllers/teachers_controller_test.rb
@@ -19,6 +19,35 @@ class TeachersControllerTest < ActionController::TestCase
     assert_template :view_game_records
     assert_template layout: "layouts/application"
   end
+
+
+   test "should get view verified students" do
+    # start the request
+    get( :view_school_verified_students)
+    assert_response :success
+    assert_not_nil assigns(:current_teacher)
+    assert_not_nil assigns(:students)
+    assert_nil assigns(:method)
+    assert_nil assigns(:order)
+    # we have only 2 verified students in the fixtures
+    assert_equal 2, assigns(:students).count
+    assert_template :view_school_verified_students
+    assert_template layout: "layouts/application"
+  end
+
+
+  test "teacher can view verified students and sort by grade asc" do
+    get( :view_school_verified_students, {'sort_method' => '1', 'order_method' => '1'})
+    assert_response :success
+    assert_not_nil assigns(:students)
+    assert_not_nil assigns(:current_teacher)
+    assert_not_nil assigns(:method)
+    assert_not_nil assigns(:order)
+    assert_equal 2, assigns(:students).count
+    assert_equal "5", assigns(:students).first.grade
+    assert_template :view_school_verified_students
+    assert_template layout: "layouts/application"
+  end
   
 
 end
