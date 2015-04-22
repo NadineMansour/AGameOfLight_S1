4657445bebc66ed68e522782502ee822365bf2dc
MMelouk94 <mmelouk94@gmail.com>
2015-04-22 10:49:19 +0200

    L3-3: Shooter, obstacle, target and light beam. No textures yet

---
+++ LightGame/Assets/Scripts/SolutionChooser_Quiz1.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SolutionChooser_Quiz1 : MonoBehaviour {
+
+
+	public GameObject correct;
+	public GameObject incorrect;
+
+
+	void OnMouseDown()
+	{
+		if(Quiz1.clickable){
+			if (tag == "Right") 
+			{
+				Quiz1.chosenSolution = "Rays";
+				Quiz1.rightAnswer = true;
+				correct.SetActive(true);
+			}
+
+
+			if(tag == "Wrong")
+			{
+				Quiz1.rightAnswer = false;
+				incorrect.SetActive(true);
+				if(name == "Cube2")
+				{
+					Quiz1.chosenSolution = "Circles";
+				}
+				if(name == "Cube3")
+				{
+					Quiz1.chosenSolution = "Cubes";
+				}
+				if(name == "Cube4")
+				{
+					Quiz1.chosenSolution = "Waves";
+				}
+			}
+
+
+			Quiz1.state++;
+		}
+	}
+}
---
+++ LightGame/Assets/Scripts/next_Quiz1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class next_Quiz1 : MonoBehaviour {
+	void OnMouseDown()
+	{
+		if(Quiz1.state == 0)
+		{
+			Quiz1.state++;
+		}
+
+
+		if(Quiz1.state == 2)
+		{
+			Application.LoadLevel("Level3");
+		}
+	}
+}
