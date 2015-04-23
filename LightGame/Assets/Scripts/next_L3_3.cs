a6854f3e130f804103ae88d528ba8db7ffdeaac4
MMelouk94 <mmelouk94@gmail.com>
2015-04-23 17:41:30 +0200

    Changes to L3_3. Detects collision with target and obstacle and reacts to them. Added tips, too. Textures will be changed

---
+++ LightGame/Assets/Scripts/next_L3_3.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class next_L3_3 : MonoBehaviour {
+
+
+	void OnMouseDown()
+	{
+		if (Shooter_L3_3.state == 3) 
+		{
+			Application.LoadLevel ("End");
+
+		} 
+		else 
+		{
+			Shooter_L3_3.state++;
+		}
+	}
+}
