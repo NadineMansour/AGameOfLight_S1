ebd0d2efba3f7b64ac573f7156acc6fc34ae4676
Nadine Mansour <nadinemansour94@gmail.com>
2015-04-23 22:17:40 +0200

    Answer a question - Refraction #135

---
+++ LightGame/Assets/Scripts/Solution_Quiz3.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Solution_Quiz3 : MonoBehaviour {
+	
+	
+	public GameObject correct;
+	public GameObject incorrect;
+	
+	
+	void OnMouseDown()
+	{
+		print (Quiz3.clickable);
+		if(Quiz3.clickable){
+			if (tag == "Right") 
+			{
+				Quiz3.chosenSolution = "Refraction";
+				Quiz3.rightAnswer = true;
+				correct.SetActive(true);
+			}
+			
+			
+			if(tag == "Wrong")
+			{
+				Quiz3.rightAnswer = false;
+				incorrect.SetActive(true);
+				if(name == "Sphere")
+				{
+					Quiz3.chosenSolution = "Reflection";
+				}
+				if(name == "Sphere 3")
+				{
+					Quiz3.chosenSolution = "Dispersion";
+				}
+				if(name == "Sphere 2")
+				{
+					Quiz3.chosenSolution = "Deviation";
+				}
+			}
+			
+			
+			Quiz3.state++;
+		}
+	}
+}
