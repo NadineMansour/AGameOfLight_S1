2d3ebf5ca78dc92e198a803b0e2be548546ae6f7
NadineMansour <nadine.mansour94@gmail.com>
2015-04-24 19:49:24 +0300

    Added quiz3 that follows the refraction levels

---
+++ LightGame/Assets/Scripts/Next_Quiz3.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Next_Quiz3 : MonoBehaviour {
+	void OnMouseDown()
+	{
+		if(Quiz3.state == 0)
+		{
+			Quiz3.state++;
+		}
+		
+		
+		if(Quiz3.state == 2)
+		{
+			Application.LoadLevel("End");
+		}
+	}
+}
