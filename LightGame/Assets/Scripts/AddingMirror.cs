97b2cef20ced1d9ad9127003908ba2aaf2b48b2d
m.badr <m.badr94@gmail.com>
2015-04-26 17:01:22 +0200

    light is interacting with ocjects

--- LightGame/Assets/Scripts/AddingMirror.cs
+++ LightGame/Assets/Scripts/AddingMirror.cs
@@ -12,15 +12,8 @@ public class AddingMirror : MonoBehaviour
 
 	void OnMouseDown() 
 	{ 
-		if (GameObject.FindGameObjectsWithTag ("Mirror").Length < MirrorCount+1) 
-		{
 			screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
 			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-		}
-		else
-		{
-			return;
-		}
 	}
 
 
