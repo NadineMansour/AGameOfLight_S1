c2df249052bba459e22993e5bf3b060e55049adb
AnasAshraf <anas.a.94.aa@gmail.com>
2015-04-27 22:56:07 +0300

    remove a teacher subject verification

--- website/app/controllers/school_admins_controller.rb
+++ website/app/controllers/school_admins_controller.rb
@@ -27,6 +27,103 @@ class SchoolAdminsController < ApplicationController
 	end
 
 
+	def reject_subject_request
+		#get the request
+		@request = TeacherRequestSubject.find(params[:teacherRequestSubject_id])
+		#change verification of request
+		@request.verified = false
+		#change the flash method depending on the save status
+		if @request.save
+			redirect_to view_subject_requests_school_admins_path, alert: 'The request has not been verified successfully.'
+			else
+			redirect_to view_subject_requests_school_admins_path, notice: 'The request has been verified successfully.'
+			end
+	end
+
+
+	def view_teachers_subjects
+		@current_school_admin = current_school_admin
+		@verified_teachers = Teacher.where("school = ? AND verified = ?", @current_school_admin.school, true)
+	end
+
+	def reject_teacher_verification
+		# find the teachers who should been verified using the id in the params
+		@teacher = Teacher.find(params[:teacher_id])
+		@teacher.verified = nil
+		# change the flash method according to the save action
+		if @teacher.save
+			redirect_to view_teacher_requests_school_admins_path, notice: 'The verification request has been removed successfully.'
+		else
+			redirect_to view_teacher_requests_school_admins_path, alert: 'The verification request has not been removed successfully.'
+		end
+	end
+
+
+	def accept_teacher_verification
+		# find the teachers who should been verified using the id in the params
+		@teacher = Teacher.find(params[:teacher_id])
+		@teacher.verified = true
+		# change the flash method according to the save action
+		if @teacher.save
+			redirect_to view_teacher_requests_school_admins_path, notice: 'The teacher has been verified successfully.'
+		else
+			redirect_to view_teacher_requests_school_admins_path, alert: 'The teacher has not been verified successfully.'
+		end
+	end
+
+
+	def remove_teacher_subject
+		@current_admin = current_school_admin
+		@request = TeacherRequestSubject.find(params[:id])
+		@request.verified = nil
+		@request.save
+		#only until method is written
+		redirect_to view_verified_requests_school_admins_path
+	end
+
+
+	def accept_subject_request
+		#get the request
+		@request = TeacherRequestSubject.find(params[:teacherRequestSubject_id])
+		#change verification of request
+		@request.verified = true
+		#change the flash method depending on the save status
+		if @request.save
+			redirect_to view_subject_requests_school_admins_path, notice: 'The request has been verified successfully.'
+		else
+			redirect_to view_subject_requests_school_admins_path, alert: 'The request has not been verified successfully.'
+		end
+	end
+
+
+	def remove_teacher_subject
+		@request = TeacherRequestSubject.find(params[:teacherRequestSubject_id])
+		@request.destroy
+		redirect_to view_teachers_subjects_school_admins_path
+	end
+
+
+	def view_subject_requests
+		#get all requests from current school
+		@allRequests = TeacherRequestSubject.where("school = ?", current_school_admin.school)
+		#filter requests to only unverified ones
+		@requests = @allRequests.where(verified: nil)
+	end
+
+
+	def view_teacher_requests
+		# get the school admin from the url till now
+		@current_admin = current_school_admin
+		if @current_admin
+			# get teachers of their school who are not verified to display their scores in the view
+			@teachers = Teacher.where("school = ? AND verified = ?" , 
+				@current_admin.school, false)
+		else
+			@teachers = {}
+		end
+	end
+
+
 	def view_students_records
 		# get the school admin from the url till now
 		@current_admin = current_school_admin
