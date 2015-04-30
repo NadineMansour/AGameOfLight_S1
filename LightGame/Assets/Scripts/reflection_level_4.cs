0e078dcdb3fe674e207801ffce43a622bef731a8
m.badr <m.badr94@gmail.com>
2015-04-30 03:32:42 +0200

    the three scripts for the reflection levels are edited

--- LightGame/Assets/Scripts/reflection_level_4.cs
+++ LightGame/Assets/Scripts/reflection_level_4.cs
@@ -1,19 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 
-
-public class reflection_level_4 : MonoBehaviour 
-{
-	//booleans to rotate left and right
+public class reflection_level_4 : MonoBehaviour {
 	public static bool rotateRight;
 	public static bool rotateLeft;
 	//variables for state recognition and calculations
 	public LineRenderer lightBeam; 
 	private List <Vector3> linePositions;
+	public LineRenderer lightBeam2;
+	private List <Vector3> linpositions2;
+	public LineRenderer lightBeam3;
+	private List <Vector3> linpositions3;
+	public LineRenderer lightBeam4;
+	private List <Vector3> linpositions4;
+
 	private float angle;
 	public static bool gameOver;
+	public static bool saveRec;
 	public GameObject target;
 	public GameObject bam;
 	public GameObject to_next_level;
@@ -27,24 +32,39 @@ public class reflection_level_4 : MonoBehaviour
 	public int score;
 	public int level;
 
-	
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		//setting light points  
 		linePositions = new List<Vector3> ();
-		Vector3 start = transform.position;
-		Vector3 mid1 = start;
-		Vector3 mid2 = start;
-		Vector3 mid3 = start;
-		Vector3 end = new Vector3(0,0,0);
-		linePositions.Add (start);
-		linePositions.Add (mid1);
-		linePositions.Add (mid2);
-		linePositions.Add (mid3);
-		linePositions.Add (end);
+		Vector3 line1Start = transform.position;
+		Vector3 line1End = new Vector3 (0,0,0);
+		linePositions.Add (line1Start);
+		linePositions.Add (line1End);
 		SetLightBeam ();
+
+		linpositions2 = new List<Vector3> ();
+		Vector3 line2Start = line1Start;
+		Vector3 line2End = line1Start;
+		linpositions2.Add (line2Start);
+		linpositions2.Add (line2End);
+		setLightBeam2 ();
+
+		linpositions3 = new List<Vector3> ();
+		Vector3 line3Start = line1Start;
+		Vector3 line3End = line1Start;
+		linpositions3.Add (line3Start);
+		linpositions3.Add (line3End);
+		setLightBeam3 ();
+
+		linpositions4 = new List<Vector3> ();
+		Vector3 line4Start = line1Start;
+		Vector3 line4End = line1Start;
+		linpositions4.Add (line4Start);
+		linpositions4.Add (line4End);
+		setLightBeam4 ();
+
 		gameOver = false;
+		saveRec = false;
 		target.SetActive (true);
 		bam.SetActive (false);
 		gameOver = false;
@@ -54,7 +74,49 @@ public class reflection_level_4 : MonoBehaviour
 		level = 4;
 		StartTime = Time.realtimeSinceStartup;
 		clicks = 0;
+		extendLightX (linePositions);
+	
 	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		TextMesh textObject = numOfClicks.GetComponent<TextMesh>();
+		textObject.text = "Moves: " + clicks;
+		if (!gameOver) {
+			if (rotateRight) {
+				RotateRight ();
+			}
+			if (rotateLeft) {
+				RotateLeft ();
+			}
+			detector ();
+			SetLightBeam ();
+			setLightBeam2 ();
+			setLightBeam3 ();
+			setLightBeam4 ();
+
+		} 
+		else 
+		{
+			EndGame();
+		}
+	}
+
+
+	void EndGame()
+	{
+		if (saveRec) 
+		{
+			StartCoroutine (save_record ()); // save the record when the game ends
+			target.SetActive (false); //enables target halo, indicatingt that light reached it
+			bam.SetActive (true); //enables target halo, indicatingt that light reached it
+			to_next_level.SetActive (true);
+			saveRec = false;
+		}
+	}
+
 
 
 	void calculateScore()
@@ -64,208 +126,149 @@ public class reflection_level_4 : MonoBehaviour
 			score = 50;
 	}
 
-
-	void EndGame()
-	{
-		StartCoroutine (save_record ()); // save the record when the game ends
-		target.SetActive (false); //enables target halo, indicatingt that light reached it
-		bam.SetActive(true); //enables target halo, indicatingt that light reached it
-		to_next_level.SetActive (true);
-	}
-	
-
-	// Update is called once per frame
-	void Update () 
-	{
-		TextMesh textObject = numOfClicks.GetComponent<TextMesh>();
-		textObject.text = "Moves: " + clicks;
-		if (!gameOver) 
-		{
-			if (rotateRight) 
-			{
-				RotateRight ();
-			}
-			if (rotateLeft)
-			{
-				RotateLeft ();
-			}
-			detector ();
-			SetLightBeam ();
-		} 
-		else 
-		{
-			EndGame();
-		}
-	}
-
-
-	public void detector()
+	void detector()
 	{
 		if (!gameOver) 
 		{
-			RaycastHit hit;
-			if (Physics.Linecast (linePositions [3], linePositions [4], out hit)) 
+			RaycastHit hit ;
+			if(Physics.Linecast(linePositions[0],linePositions[1],out hit))
 			{
-				// check if the light hitted the target
-				if (hit.collider.tag == "Target") 
-				{
-					gameOver = true;
-					linePositions [4] = hit.point;
-					FinishTime = Time.realtimeSinceStartup;
-					timeInLevel = (int)FinishTime - (int)StartTime;
-					calculateScore (); //score now has its right value
-					//Mariam, at this point -> Clicks, score, timeInLevel, log & level are all ready. Good luck
-					//Insert Post records method here.
-					StartCoroutine (save_record ());
-				}
+				Vector3 collision = hit.point;
 
 
-				//check if the light hitted an obstacle
-				if (hit.collider.tag == "Obstacle") 
-				{
-					linePositions [4] = hit.point;
-				}
-
-
-				// check if the light hitted a horizontal mirror the reflection is done on the x-axis
-				if (hit.collider.tag == "horizontal mirror")
-				{
-					Vector3 collision = hit.point;
-					float x = 0;
-					// this condition is for checking if there is no collisons at all. the three mid point are still at the start point of the line
-					//if no collison occurs point 4 is = to the point of collison
-					if (linePositions [3] == linePositions [0]) 
-					{
-						linePositions [3] = collision;
-						x = 2*(collision.x - linePositions [0].x)+linePositions[0].x;
-						linePositions [4] = new Vector3 (x, linePositions[0].y, 0);
-
-					}
-					else
-					{
-						//this condition is checking if there is only one collision. two midpoints are at the start point
-						//if there is one collision only and another mirror is added point 3 is = to point 4 and point 4 is = to point of collision
-						if (linePositions [2] == linePositions [0]) 
-						{
-							linePositions [2] = linePositions [3];
-							linePositions [3] = collision;
-							x = 2*(collision.x - linePositions [2].x)+linePositions[2].x;
-							linePositions [4] = new Vector3 (x, linePositions[2].y, 0);
-						}
-						else
-						{
-							//this condition if there is two collision points. one point is at the start point 
-							//if there is two collision points and a third mirror is add point 2 is = to point 3 and point 3 is = to point 4 and point 4 is = to collision point
-							if (linePositions [1] == linePositions [0]) 
-							{
-								linePositions [1] = linePositions [2];
-								linePositions [2] = linePositions [3];
-								linePositions [3] = collision;
-								x = 2*(collision.x - linePositions [2].x) + linePositions[2].x;
-								linePositions [4] = new Vector3 (x, linePositions[2].y, 0);
-							}
-						}
-					}	 
-
-					extendLightX();
-				}
-
-
-				//check if the light hitted a vertical mirror the reflection is done on y-axis
-				//this condition have the same idea of the horizontal mirror
-				if (hit.collider.tag == "vertical mirror") 
-				{
-					Vector3 collision = hit.point;
-					float y = 0;
-					if (linePositions [3] == linePositions [0]) 
-					{
-						linePositions [3] = collision;
-						y = 2*(linePositions[0].y - collision.y) + linePositions[0].y;
-						linePositions [4] = new Vector3 (linePositions[0].x, -y, 0);
-					}
-					else 
-					{
-						if (linePositions [2] == linePositions [0]) 
-						{
-							linePositions [2] = linePositions [3];
-							linePositions [3] = collision;
-							y = 2*(linePositions[2].y - collision.y)+linePositions[2].y;
-							linePositions [4] = new Vector3 (linePositions[2].x, -y, 0);
-						}
-						else 
-						{
-							if (linePositions [1] == linePositions [0]) 
-							{
-								linePositions [1] = linePositions [2];
-								linePositions [2] = linePositions [3];
-								linePositions [3] = collision;
-								y = 2*(linePositions[2].y - collision.y) + linePositions[2].y;
-								linePositions [4] = new Vector3 (linePositions[2].x, -y, 0);
-							}
-						}
-					}
-					extendLightY();
-				}
-			}
-
-
-			//to be continued***************************** 
-			/*else 
-				if (Physics.Linecast(linePositions[2],linePositions[3],out hit))
-			{
+				
+				linePositions[1] = collision;
+				linpositions2[0] = collision;
 				if(hit.collider.tag == "horizontal mirror")
 				{
-					Vector3 collision = hit.point;
-					float x = 0;
-
-					if(linePositions[2] == linePositions[0])
-					{
-						linePositions[3] = collision;
-						x = 2*(collision.x - linePositions [0].x)+linePositions[0].x;
-						linePositions [4] = new Vector3 (x, linePositions[0].y, 0);
-					} 
-					else if (linePositions[1] ==  linePositions[0])
-					{
-						linePositions[3] = collision;
-						x = 2*(collision.x - linePositions [0].x)+linePositions[0].x;
-						linePositions [4] = new Vector3 (x, linePositions[0].y, 0);
-					}
-
+					
+					float x = 2*(collision.x - linePositions [0].x)+linePositions[0].x;
+					linpositions2 [1] = new Vector3 (x, linePositions[0].y, 0);
+					extendLightX(linpositions2);
 				}
-			}*/
-		}
-	}
+				else
+				{
+					if(hit.collider.tag == "vertical mirror")
+					{
+						float y = 2*(linePositions[0].y - collision.y) ;
+						linpositions2[1] = new Vector3(linePositions[0].x,-y + linePositions[0].y,0);
+						extendLightY(linpositions2);
+						extendLightX(linpositions2);
+					}
+				}
 
-
-	void extendLightX()
-	{
-		if (linePositions [4].y < 6) 
-		{
-			Vector3 point1 = linePositions [3];
-			Vector3 point2 = linePositions [4];
-			float slope = (point2.y - point1.y) / (point2.x - point1.x);
-			float yIntercept = point1.y - slope * point1.x;
-			if(linePositions[3].x < linePositions[4].x)
+				if(hit.collider.tag == "Obstacle")
+				{
+					linpositions2[1] = hit.point;
+				}
+			}
+			else 
 			{
-				linePositions [4] = new Vector3 (15, 15 * slope + yIntercept, 0);
+				linpositions2[0] = linePositions[0];
+				linpositions2[1] = linePositions[0];
+				extendLightX(linePositions);
+
+			}
+
+
+			if(Physics.Linecast(linpositions2[0],linpositions2[1],out hit))
+			{
+				Vector3 collision = hit.point;
+				
+				
+				linpositions2[1] = collision;
+				linpositions3[0] = collision;
+				if(hit.collider.tag == "horizontal mirror")
+				{
+					float x = 2*(collision.x - linpositions2[0].x) + linpositions2[0].x;
+					linpositions3[1] = new Vector3(x,linpositions2[0].y,0);
+					extendLightX(linpositions3);
+				}
+				else 
+				{
+					if(hit.collider.tag == "vertical mirror")
+					{
+						float y = 2*(linpositions2[0].y - collision.y) ;
+						linpositions3[1] = new Vector3(linpositions2[0].x,-y + linpositions2[0].y,0);
+						extendLightY(linpositions3);
+						extendLightX(linpositions3);
+					} 
+				}
+				
+				if(hit.collider.tag == "Obstacle")
+				{
+					linpositions3[1] = collision;
+				}
+				if(hit.collider.tag == "Target")
+				{
+					gameOver = true;
+					saveRec = true;
+					linpositions3[1] = collision;
+				}
+			}
+			else 
+			{
+				linpositions3[0] = linePositions[0];
+				linpositions3[1] = linePositions[0];
+				extendLightX(linpositions3);
+			}
+
+
+			if(Physics.Linecast(linpositions3[0],linpositions3[1],out hit))
+			{
+				Vector3 collision = hit.point;
+				linpositions3[1] = collision;
+				linpositions4[0] = collision;
+				if(hit.collider.tag == "horizontal mirror")
+				{
+					
+					float x = 2*(collision.x - linpositions3[0].x) + linpositions3[0].x;
+					linpositions4[1] = new Vector3(x,linpositions3[0].y,0);
+					extendLightX(linpositions4);
+				}
+				else
+				{
+					if(hit.collider.tag == "vertical mirror")
+					{
+						float y = 2*(linpositions3[0].y - collision.y) ;
+						linpositions4[1] = new Vector3(linpositions3[0].x,-y + linpositions3[0].y	,0);
+						extendLightY(linpositions4);
+					}
+				}
+				
+				if(hit.collider.tag == "Obstacle")
+				{
+					linpositions4[1] = hit.point;
+				}
+
+				if(hit.collider.tag == "Target")
+				{
+					gameOver = true;
+					saveRec = true;
+					linpositions4[1] = collision;
+				}
 			}
 			else
 			{
-				linePositions[4] = new Vector3 (-15,-15*slope + yIntercept,0);
+				linpositions4[0] = linePositions[0];
+				linpositions4[1] = linePositions[0];
 			}
-		}
-	}
 
 
-	void extendLightY()
-	{
-		if (linePositions [4].y < 3) 
-		{
-			Vector3 point1 = linePositions[3];
-			Vector3 point2 = linePositions[4];
-			float slope = (point2.y - point1.y)/(point2.x-point1.x);
-			float yIntercept = point1.y - slope * point1.x;
-			linePositions[4] = new Vector3((-5-yIntercept)/slope,-5,0);
+			if(Physics.Linecast(linpositions4[0] , linpositions4[1] ,out hit))
+			{
+				if (hit.collider.tag == "Target")
+				{
+					gameOver = true;
+					saveRec = true;
+					linpositions4[1] = hit.point;
+				}
+
+				if(hit.collider.tag == "Obstacle")
+				{
+					linpositions4[1] = hit.point;
+				}
+			}
 		}
 	}
 
@@ -277,8 +280,34 @@ public class reflection_level_4 : MonoBehaviour
 			lightBeam.SetPosition(i, linePositions[i]);
 		}
 	}
-	
-	
+
+	void setLightBeam2()
+	{
+		for (int i = 0; i < linpositions2.Count; i++) 
+		{
+			lightBeam2.SetPosition(i,linpositions2[i]);
+		}
+	}
+
+
+	void setLightBeam3()
+	{
+		for (int i = 0; i < linpositions3.Count; i++) 
+		{
+			lightBeam3.SetPosition(i,linpositions3[i]);
+		}
+	}
+
+
+	void setLightBeam4()
+	{
+		for (int i = 0; i < linpositions4.Count; i++) 
+		{
+			lightBeam4.SetPosition(i,linpositions4[i]);
+		}
+	}
+
+
 	public static void RotateRightTrue() // the method sets the RotateRight value to true
 	{
 		if (!rotateRight) 
@@ -324,7 +353,7 @@ public class reflection_level_4 : MonoBehaviour
 		}
 		
 	}
-
+	
 	
 	void RotateLeft()
 	{
@@ -336,50 +365,82 @@ public class reflection_level_4 : MonoBehaviour
 			RotateLightBeam();
 		}
 	}
-	
 
-	void PointRotator() // the method rotate all point in the line in a straight line, and the reflection is done by the detector method.
+	void extendLightX(List<Vector3> l)
+	{
+		if (l [1].y < 6) 
+		{
+			Vector3 point1 = l [0];
+			Vector3 point2 = l [1];
+			float slope = (point2.y - point1.y) / (point2.x - point1.x);
+			//l [1] = new Vector3 (8, (slope*8)-(slope*l[0].x)+l[0].y, 0);
+			if(l[0].x < l[1].x)
+			{
+				//linePositions [4] = new Vector3 (15, 15 * slope + yIntercept, 0);
+				l [1] = new Vector3 (8, (slope*8)-(slope*l[0].x)+l[0].y, 0);
+			}
+			else
+			{
+				l[1] = new Vector3 (-8,(-8*slope)- (slope * l[0].x) + l[0].y,0);
+			}
+		}
+	}
+
+
+	void extendLightY(List <Vector3> l)
+	{
+		if (l [1].y < 5) 
+		{
+			Vector3 point1 = l[0];
+			Vector3 point2 = l[1];
+			float slope = (point2.y - point1.y)/(point2.x-point1.x);
+			//l[1] = new Vector3((-5+(slope*l[0].x)-l[0].y)/slope,-5,0);
+			if(l[0].y > l[1].y)
+			{
+				l[1] = new Vector3((-5+(slope*l[0].x)-l[0].y)/slope,-5,0);
+			}
+			else 
+			{
+				l[1] = new Vector3((5+(slope*l[0].x)-l[0].y)/slope,5,0);
+			}
+		}
+	}
+
+
+	void PointRotator()
 	{
 		Vector3 pivotPoint = linePositions [0];
 		Vector3 pointToRotate = new Vector3 (0, pivotPoint.y, 0);
 		float Nx = (pointToRotate.x - pivotPoint.x);
 		float Ny = (pointToRotate.y - pivotPoint.y);
 		float angle1 = angle * Mathf.PI / 180.0f;
-		linePositions[4] = new Vector3((float)(Mathf.Cos(angle1) * Nx - Mathf.Sin(angle1) * Ny + pivotPoint.x), (float)(Mathf.Sin(angle1) * Nx + Mathf.Cos(angle1) * Ny + pivotPoint.y), 0);
+		linePositions[1] = new Vector3((float)(Mathf.Cos(angle1) * Nx - Mathf.Sin(angle1) * Ny + pivotPoint.x), (float)(Mathf.Sin(angle1) * Nx + Mathf.Cos(angle1) * Ny + pivotPoint.y), 0);
 
+		linpositions2 [0] = linePositions [0];
+		linpositions2 [1] = linePositions [0];
 
-		Vector3 midpoint1 = new Vector3 (linePositions [0].x, 0, 0);
-		float m1x = (midpoint1.x - pivotPoint.x);
-		float m1y = (midpoint1.x - pivotPoint.x);
-		linePositions[1] = new Vector3((float)(Mathf.Cos(angle1) * m1x - Mathf.Sin(angle1) * m1y + pivotPoint.x), (float)(Mathf.Sin(angle1) * m1x + Mathf.Cos(angle1) * m1y + pivotPoint.y), 0);
+		linpositions3 [0] = linePositions [0];
+		linpositions3 [1] = linePositions [0];
 
+		linpositions4 [0] = linePositions [0];
+		linpositions4 [1] = linePositions [0];
 
-		Vector3 midpoint2 = new Vector3 (linePositions [1].x, 0, 0);
-		float m2x = (midpoint2.x - pivotPoint.x);
-		float m2y = (midpoint2.y - pivotPoint.y);
-		linePositions[2] = new Vector3((float)(Mathf.Cos(angle1) * m2x - Mathf.Sin(angle1) * m2y + pivotPoint.x), (float)(Mathf.Sin(angle1) * m2x + Mathf.Cos(angle1) * m2y + pivotPoint.y), 0);
-
-
-		Vector3 midpoint3 = new Vector3(linePositions[2].x,0,0);
-		float m3x = (midpoint3.x - pivotPoint.x);
-		float m3y = (midpoint3.y - pivotPoint.y);
-		linePositions[3] = new Vector3((float)(Mathf.Cos(angle1) * m3x - Mathf.Sin(angle1) * m3y + pivotPoint.x), (float)(Mathf.Sin(angle1) * m3x + Mathf.Cos(angle1) * m3y + pivotPoint.y), 0);
-	
-
-		extendLightX ();
+		extendLightX(linePositions);
 	}
 
-	
 	void RotateLightBeam() //method that apply rotation
 	{
 		PointRotator ();
 		SetLightBeam ();
+		setLightBeam2 ();
+		setLightBeam3 ();
+		setLightBeam4 ();
 	}
 
 
 	IEnumerator save_record() 
 	{
-		string urlMessage = "https://ilearn-td.herokuapp.com/api/records/save_record";
+		string urlMessage = "https://k12-mariammohamed.c9.io/api/records/save_record";
 		WWWForm form = new WWWForm ();
 		// pass the email authentication
 		string user_email = ButtonLogin.user_email;
@@ -403,5 +464,3 @@ public class reflection_level_4 : MonoBehaviour
 		}
 	}
 }
-
-
