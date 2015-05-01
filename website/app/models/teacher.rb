76007df62cc60d04758ff6ed8d6f296c8b63f50d
mohamed raouf mohamed <mido_2ofa@hotmail.com>
2015-05-01 03:22:07 +0300

    adding a sign in buttons to teacher and school admin and system admin , teacher can add a question to a subject if only he is verified to this subject , fixing the view

--- website/app/models/teacher.rb
+++ website/app/models/teacher.rb
@@ -3,8 +3,9 @@ class Teacher < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
+ before_save { |teacher| teacher.school.downcase! }
   has_many :teacher_request_subjects
   has_many :questions
+  has_many :teachersubjects
+  has_many :subjects, through: :teacher_request_subjects
 end
